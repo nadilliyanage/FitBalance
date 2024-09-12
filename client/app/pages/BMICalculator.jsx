88a85fc0065684b91cfad93ddf44d9aa7f1f4aba
HeshanChamika <it22194930@my.sliit.lk>
2024-09-12 10:10:04 +0530

    Refactor BMIResults and BMIModal components

--- client/app/pages/BMICalculator.jsx
+++ client/app/pages/BMICalculator.jsx
@@ -1,30 +1,42 @@
-import React, { useState, useCallback } from 'react';
-import { View, Text, TouchableOpacity, ScrollView, Dimensions, TextInput, Modal, Alert } from 'react-native';
+import React, { useState, useCallback, lazy, Suspense } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  ScrollView,
+  Dimensions,
+  TextInput,
+  Alert,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { FontAwesome5 } from '@expo/vector-icons'; // For the gender icons and increment/decrement buttons
-import { BlurView } from 'expo-blur'; // For blurring the background
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
+import BMIModal from "./BMIModal"; // Import the BMIModal component
 
-const { width } = Dimensions.get('window'); // Get the device's width
+const LazyExercices = lazy(() => import("../(tabs)/Exercises"));
+
+const { width } = Dimensions.get("window");
 
 const BMICalculator = () => {
-  const [gender, setGender] = useState('male');
-  const [weight, setWeight] = useState('65');
-  const [age, setAge] = useState('26');
-  const [height, setHeight] = useState('170'); // Default height value as string
+  const [gender, setGender] = useState("male");
+  const [weight, setWeight] = useState("50");
+  const [age, setAge] = useState("20");
+  const [height, setHeight] = useState("100");
   const [isModalVisible, setModalVisible] = useState(false);
   const [bmiResult, setBmiResult] = useState(null);
+  const [back, setBack] = useState(false);
 
-  // Throttle the onScroll event to prevent too many updates
   const handleScroll = useCallback((event) => {
     const scrollPosition = event.nativeEvent.contentOffset.x;
-    const newHeight = Math.min(Math.max(Math.round(scrollPosition / 10) + 100, 100), 220);
+    const newHeight = Math.min(
+      Math.max(Math.round(scrollPosition / 10) + 100, 100),
+      220
+    );
     setHeight(newHeight.toString());
   }, []);
 
-  // Function to calculate BMI
   const calculateBMI = () => {
     const weightNum = parseFloat(weight);
-    const heightNum = parseFloat(height) / 100; // Convert height to meters
+    const heightNum = parseFloat(height) / 100;
     if (weightNum > 0 && heightNum > 0) {
       const bmi = (weightNum / (heightNum * heightNum)).toFixed(1);
       setBmiResult({
@@ -34,58 +46,132 @@ const BMICalculator = () => {
         age,
         gender,
       });
-      setModalVisible(true); // Show the modal with the BMI result
+      setModalVisible(true);
     } else {
-      Alert.alert('Invalid Input', 'Please enter valid weight and height.');
+      Alert.alert("Invalid Input", "Please enter valid weight and height.");
     }
   };
+
+  if (back) {
+    return (
+      <Suspense fallback={<Text>Loading...</Text>}>
+        <LazyExercices />
+      </Suspense>
+    );
+  }
+
+  const pointerLeft = (parseInt(height) - 100) * 10 + width / 2 - 15;
 
   return (
     <SafeAreaView className="bg-white flex-1">
       <ScrollView>
         <View className="p-5">
-          {/* Header */}
-          <Text className="text-xl font-bold mb-5 text-center">BMI Calculator</Text>
+          <View className="flex-row items-center justify-center mb-5">
+            <TouchableOpacity
+              style={{ position: "absolute", left: 10 }}
+              onPress={() => setBack(true)}
+            >
+              <FontAwesome5 name="arrow-left" size={20} color="purple" />
+            </TouchableOpacity>
+            <Text className="text-xl font-bold">BMI Calculator</Text>
+          </View>
 
-          {/* Gender Selection */}
-          <Text className="text-lg font-semibold mb-3">Please choose your gender</Text>
+          <Text className="text-lg font-semibold mb-3">
+            Please choose your gender
+          </Text>
           <View className="flex-row justify-center mb-5">
             <TouchableOpacity
-              onPress={() => setGender('male')}
-              className={`flex-1 items-center py-3 mx-2 rounded-lg ${gender === 'male' ? 'bg-purple-200' : 'bg-gray-200'}`}
+              onPress={() => setGender("male")}
+              className={`flex-1 items-center py-3 mx-2 rounded-lg ${
+                gender === "male" ? "bg-purple-200" : "bg-gray-200"
+              }`}
+              style={{ position: "relative" }}
             >
-              <FontAwesome5 name="mars" size={40} color={gender === 'male' ? 'blue' : 'gray'} />
+              <View
+                style={{
+                  position: "absolute",
+                  top: -10,
+                  left: -10,
+                  width: 30,
+                  height: 30,
+                  borderRadius: 15,
+                  backgroundColor: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <FontAwesome5
+                  name="mars"
+                  size={18}
+                  color={gender === "male" ? "blue" : "gray"}
+                />
+              </View>
+              <MaterialCommunityIcons
+                name="face-man"
+                size={40}
+                color={gender === "male" ? "blue" : "gray"}
+              />
             </TouchableOpacity>
 
             <TouchableOpacity
-              onPress={() => setGender('female')}
-              className={`flex-1 items-center py-3 mx-2 rounded-lg ${gender === 'female' ? 'bg-purple-200' : 'bg-gray-200'}`}
+              onPress={() => setGender("female")}
+              className={`flex-1 items-center py-3 mx-2 rounded-lg ${
+                gender === "female" ? "bg-purple-200" : "bg-gray-200"
+              }`}
+              style={{ position: "relative" }}
             >
-              <FontAwesome5 name="venus" size={40} color={gender === 'female' ? 'red' : 'gray'} />
+              <View
+                style={{
+                  position: "absolute",
+                  top: -10,
+                  left: -10,
+                  width: 30,
+                  height: 30,
+                  borderRadius: 15,
+                  backgroundColor: "white",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <FontAwesome5
+                  name="venus"
+                  size={18}
+                  color={gender === "female" ? "red" : "gray"}
+                />
+              </View>
+              <MaterialCommunityIcons
+                name="face-woman"
+                size={40}
+                color={gender === "female" ? "red" : "gray"}
+              />
             </TouchableOpacity>
           </View>
 
-          {/* Modify Values Section */}
-          <Text className="text-lg font-semibold mb-3">Please Modify the values</Text>
+          <Text className="text-lg font-semibold mb-3">
+            Please Modify the values
+          </Text>
           <View className="flex-row justify-between mb-5">
-            {/* Weight */}
             <View className="items-center bg-purple-200 w-36 py-5 rounded-lg">
               <Text className="text-lg font-semibold mb-2">Weight (kg)</Text>
               <TextInput
-                style={{ fontSize: 30, fontWeight: 'bold' }}
+                style={{ fontSize: 30, fontWeight: "bold" }}
                 keyboardType="numeric"
                 value={weight}
-                onChangeText={text => setWeight(text)}
+                onChangeText={(text) => setWeight(text)}
               />
               <View className="flex-row mt-2">
                 <TouchableOpacity
-                  onPress={() => setWeight((prev) => (parseInt(prev) - 1).toString())}
+                  onPress={() =>
+                    setWeight((prev) => (parseInt(prev) - 1).toString())
+                  }
                   className="bg-white p-2 rounded-full mx-2"
                 >
                   <FontAwesome5 name="minus" size={20} />
                 </TouchableOpacity>
                 <TouchableOpacity
-                  onPress={() => setWeight((prev) => (parseInt(prev) + 1).toString())}
+                  onPress={() =>
+                    setWeight((prev) => (parseInt(prev) + 1).toString())
+                  }
                   className="bg-white p-2 rounded-full mx-2"
                 >
                   <FontAwesome5 name="plus" size={20} />
@@ -93,24 +179,27 @@ const BMICalculator = () => {
               </View>
             </View>
 
-            {/* Age */}
             <View className="items-center bg-purple-200 w-36 py-5 rounded-lg">
               <Text className="text-lg font-semibold mb-2">Age</Text>
               <TextInput
-                style={{ fontSize: 30, fontWeight: 'bold' }}
+                style={{ fontSize: 30, fontWeight: "bold" }}
                 keyboardType="numeric"
                 value={age}
-                onChangeText={text => setAge(text)}
+                onChangeText={(text) => setAge(text)}
               />
               <View className="flex-row mt-2">
                 <TouchableOpacity
-                  onPress={() => setAge((prev) => (parseInt(prev) - 1).toString())}
+                  onPress={() =>
+                    setAge((prev) => (parseInt(prev) - 1).toString())
+                  }
                   className="bg-white p-2 rounded-full mx-2"
                 >
                   <FontAwesome5 name="minus" size={20} />
                 </TouchableOpacity>
                 <TouchableOpacity
-                  onPress={() => setAge((prev) => (parseInt(prev) + 1).toString())}
+                  onPress={() =>
+                    setAge((prev) => (parseInt(prev) + 1).toString())
+                  }
                   className="bg-white p-2 rounded-full mx-2"
                 >
                   <FontAwesome5 name="plus" size={20} />
@@ -119,78 +208,79 @@ const BMICalculator = () => {
             </View>
           </View>
 
-          {/* Draggable Height Ruler */}
           <View className="bg-purple-200 py-5 mb-5 rounded-lg items-center">
             <Text className="text-lg font-semibold mb-2">Height (cm)</Text>
             <TextInput
-              style={{ fontSize: 30, fontWeight: 'bold', marginBottom: 10 }}
+              style={{ fontSize: 30, fontWeight: "bold", marginBottom: 10 }}
               keyboardType="numeric"
               value={height}
-              onChangeText={text => setHeight(text)}
+              onChangeText={(text) => setHeight(text)}
             />
 
-            {/* Scrollable Ruler */}
             <ScrollView
               horizontal
               showsHorizontalScrollIndicator={false}
               onScroll={handleScroll}
               scrollEventThrottle={16}
-              contentOffset={{ x: (parseInt(height) - 100) * 10 }}
               decelerationRate="fast"
               snapToInterval={10}
               bounces={false}
             >
-              <View style={{ flexDirection: 'row', paddingHorizontal: width / 2 - 20 }}>
-                {Array.from({ length: 121 }).map((_, index) => (
-                  <View key={index} style={{ alignItems: 'center', marginRight: 10 }}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  paddingHorizontal: width / 2 - 40,
+                }}
+              >
+                {Array.from({ length: 121 }).map((_, i) => (
+                  <View key={i} className="mx-2 items-center">
                     <View
                       style={{
-                        width: 2,
-                        height: index % 5 === 0 ? 30 : 15,
-                        backgroundColor: index % 5 === 0 ? '#000' : '#888',
+                        height: i % 5 === 0 ? 40 : 20,
+                        width: 3,
+                        backgroundColor: "black",
                       }}
                     />
-                    {index % 10 === 0 && (
-                      <Text style={{ marginTop: 5 }}>{100 + index}</Text>
+                    {i % 5 === 0 && (
+                      <Text className="mt-2 font-semibold">{i + 100}</Text>
                     )}
                   </View>
                 ))}
               </View>
             </ScrollView>
-          </View>
 
-          {/* Calculate Button */}
-          <TouchableOpacity className="bg-purple-500 py-4 rounded-lg" onPress={calculateBMI}>
-            <Text className="text-center text-white text-lg font-bold">Calculate</Text>
-          </TouchableOpacity>
-        </View>
-
-        {/* Modal for BMI Results */}
-        <Modal
-          visible={isModalVisible}
-          transparent={true}
-          animationType="fade"
-          onRequestClose={() => setModalVisible(false)}
-        >
-          <View className="flex-1 justify-center items-center">
-            <BlurView intensity={100} className="absolute inset-0" />
-            <View className="bg-white rounded-lg p-10 mx-5">
-              <Text className="text-lg font-semibold mb-3 text-center">BMI Results</Text>
-              <Text className="text-base mb-1">BMI: {bmiResult?.bmi}</Text>
-              <Text className="text-base mb-1">Gender: {bmiResult?.gender}</Text>
-              <Text className="text-base mb-1">Age: {bmiResult?.age}</Text>
-              <Text className="text-base mb-1">Height: {bmiResult?.height} cm</Text>
-              <Text className="text-base mb-4">Weight: {bmiResult?.weight} kg</Text>
-              <TouchableOpacity
-                className="bg-secondary py-2 rounded-lg"
-                onPress={() => setModalVisible(false)}
-              >
-                <Text className="text-center text-white">Close</Text>
-              </TouchableOpacity>
+            <View
+              style={{
+                position: "absolute",
+                top: 130,
+                bottom: 0,
+                width: 30,
+                height: 30,
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <FontAwesome5 name="arrow-up" size={24} color="purple" />
             </View>
           </View>
-        </Modal>
+
+          <TouchableOpacity
+            onPress={calculateBMI}
+            className="bg-purple-500 py-4 rounded-lg"
+          >
+            <Text className="text-center text-white font-bold text-lg">
+              Calculate BMI
+            </Text>
+          </TouchableOpacity>
+        </View>
       </ScrollView>
+
+      {/* Use the BMIModal component */}
+      <BMIModal
+        visible={isModalVisible}
+        onClose={() => setModalVisible(false)}
+        bmiResult={bmiResult}
+      />
     </SafeAreaView>
   );
 };
---
+++ client/app/pages/BMIModal.jsx
@@ -0,0 +1,256 @@
+import React, { useEffect, useRef } from "react";
+import { View, Text, TouchableOpacity, Modal, Animated } from "react-native";
+import { BlurView } from "expo-blur";
+import Icon from 'react-native-vector-icons/FontAwesome'; // Ensure this library is installed
+
+const BMIModal = ({ visible, onClose, bmiResult, navigation }) => {
+  const moveValue = useRef(new Animated.Value(0)).current;
+
+  // Define the horizontal movement animation
+  const moveAnimation = Animated.loop(
+    Animated.sequence([
+      Animated.timing(moveValue, {
+        toValue: 10, // Move 10 units to the right
+        duration: 500, // Duration for the movement
+        useNativeDriver: true,
+      }),
+      Animated.timing(moveValue, {
+        toValue: 0, // Return to the original position
+        duration: 500, // Duration for the return
+        useNativeDriver: true,
+      }),
+    ])
+  );
+
+  // Start the animation when the component mounts
+  useEffect(() => {
+    moveAnimation.start();
+  }, [moveAnimation]);
+
+  // Function to determine the BMI category based on BMI value
+  const getBMICategory = (bmi) => {
+    if (bmi < 18.5) return "Underweight";
+    if (bmi < 25) return "Normal weight";
+    if (bmi < 30) return "Overweight";
+    return "Obesity";
+  };
+
+  // Function to get color based on BMI category
+  const getCategoryColor = (category) => {
+    switch (category) {
+      case "Underweight":
+        return "#007bff"; // Blue
+      case "Normal weight":
+        return "#28a745"; // Green
+      case "Overweight":
+        return "#ffc107"; // Yellow
+      case "Obesity":
+        return "#dc3545"; // Red
+      default:
+        return "#ffffff"; // Default to white
+    }
+  };
+
+  // Function to get chat bubble style
+  const getChatBubbleStyle = (category) => {
+    return {
+      padding: 10,
+      borderRadius: 8,
+      backgroundColor: getCategoryColor(category),
+      borderColor: getCategoryColor(category),
+      borderWidth: 1,
+      position: "relative",
+      marginTop: 10, // Adjusted to ensure space below BMI value
+    };
+  };
+
+  // Function to calculate the healthy weight range
+  const calculateHealthyWeightRange = (height) => {
+    // Convert height from cm to meters
+    const heightInMeters = height / 100;
+    // Calculate weight range based on BMI values of 18.5 to 24.9
+    const minWeight = 18.5 * heightInMeters * heightInMeters;
+    const maxWeight = 24.9 * heightInMeters * heightInMeters;
+    return { minWeight: Math.round(minWeight), maxWeight: Math.round(maxWeight) };
+  };
+
+  // Determine the BMI category and color
+  const bmiCategory = bmiResult ? getBMICategory(parseFloat(bmiResult.bmi)) : "";
+  const chatBubbleStyle = getChatBubbleStyle(bmiCategory);
+
+  // Message based on BMI category
+  const categoryMessage = {
+    Underweight:
+      "You are underweight. Consider consulting with a healthcare provider.",
+    "Normal weight":
+      "Your weight is normal. Keep maintaining a healthy lifestyle.",
+    Overweight:
+      "You are overweight. It might be beneficial to review your diet and exercise.",
+    Obesity:
+      "You are in the obesity range. It's important to consult with a healthcare provider for advice.",
+  };
+
+  // Determine the font color for the BMI value
+  const bmiFontColor = getCategoryColor(bmiCategory);
+
+  // Calculate healthy weight range
+  const healthyWeightRange = bmiResult ? calculateHealthyWeightRange(bmiResult.height) : { minWeight: 0, maxWeight: 0 };
+
+  // Navigation function
+  const handleNavigate = () => {
+    navigation.navigate("ExercisePage"); // Replace "ExercisePage" with your actual route name
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      transparent={true}
+      animationType="fade"
+      onRequestClose={onClose}
+    >
+      <BlurView
+        tint="light"
+        intensity={100}
+        style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+      >
+        <View
+          style={{
+            backgroundColor: "white",
+            padding: 16,
+            borderRadius: 8,
+            width: 320,
+            position: 'relative', // Ensure chat bubble is positioned relative to the modal content
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              fontSize: 24,
+              fontWeight: "bold",
+              marginBottom: 16,
+            }}
+          >
+            Your BMI:
+          </Text>
+          {bmiResult && (
+            <View>
+              <Text
+                style={{
+                  textAlign: "center",
+                  fontSize: 48,
+                  fontWeight: "bold",
+                  marginBottom: 8,
+                  color: bmiFontColor, // Set the BMI font color based on category
+                }}
+              >
+                {bmiResult.bmi}
+              </Text>
+
+              {/* BMI Category Message */}
+              <View style={{ position: "relative", marginBottom: 16 }}>
+                <View style={chatBubbleStyle}>
+                  <Text style={{ color: "white", textAlign: "center" }}>
+                    {categoryMessage[bmiCategory]}
+                  </Text>
+                </View>
+
+                {/* BMI Category Bar */}
+                <View style={{ marginTop: 20 }}>
+                  <View style={{ height: 10, flexDirection: "row" }}>
+                    <View style={{ flex: 1, backgroundColor: "#007bff" }} />
+                    <View style={{ flex: 2, backgroundColor: "#28a745" }} />
+                    <View style={{ flex: 1, backgroundColor: "#ffc107" }} />
+                    <View style={{ flex: 2, backgroundColor: "#dc3545" }} />
+                  </View>
+                </View>
+              </View>
+
+              {/* Display weight and height on the left side, age and gender on the right */}
+              <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 16 }}>
+                <View style={{ flex: 1 }}>
+                  <Text style={{ textAlign: "left", fontSize: 18, marginBottom: 8 }}>
+                    Weight: {bmiResult.weight} kg
+                  </Text>
+                  <Text style={{ textAlign: "left", fontSize: 18, marginBottom: 8 }}>
+                    Height: {bmiResult.height} cm
+                  </Text>
+                </View>
+                <View style={{ flex: 1 }}>
+                  <Text style={{ textAlign: "right", fontSize: 18, marginBottom: 8 }}>
+                    Age: {bmiResult.age} years
+                  </Text>
+                  <Text style={{ textAlign: "right", fontSize: 18 }}>
+                    Gender: {bmiResult.gender}
+                  </Text>
+                </View>
+              </View>
+
+              {/* Healthy Weight Range */}
+              <View style={{ marginBottom: 8}}>
+                <Text style={{ textAlign: "center", fontSize: 18, marginBottom: 8, fontWeight: '600' }}>
+                  Healthy Weight Range for Your Height:
+                </Text>
+                <Text style={{ textAlign: "center", fontSize: 22, fontWeight: 'bold' }}>
+                  {healthyWeightRange.minWeight} - {healthyWeightRange.maxWeight} kg
+                </Text>
+              </View>
+
+            </View>
+          )}
+
+          {/* Navigate Button */}
+          <TouchableOpacity
+            onPress={handleNavigate}
+            style={{
+              backgroundColor: "#28a745",
+              paddingVertical: 8,
+              borderRadius: 8,
+              marginTop: 16,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}
+          >
+            <Text
+              style={{
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 18,
+                marginRight: 8
+              }}
+            >
+              Let's Start Journey
+            </Text>
+            <Animated.View style={{ transform: [{ translateX: moveValue }] }}>
+              <Icon name="arrow-right" size={20} color="white" />
+            </Animated.View>
+          </TouchableOpacity>
+
+          {/* Close Button */}
+          <TouchableOpacity
+            onPress={onClose}
+            style={{
+              backgroundColor: "#6a0dad",
+              paddingVertical: 8,
+              borderRadius: 8,
+              marginTop: 16,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "white",
+                fontWeight: "bold",
+                fontSize: 18,
+              }}
+            >
+              Close
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </BlurView>
+    </Modal>
+  );
+};
+
+export default BMIModal;
